a9b120633104cff5a44e75213ea5ebe204b5c3b8
Blackcode2 <doky03115@gmail.com>
2024-11-07 20:16:20 +0900

    [Fix]: delete function of button

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,6 @@
 import "./Button.css";
 
 function Button() {
-  let button = document.getElementById("btn");
-
-  button.addEventListener("click", function () {
-    alert("Button Clicked");
-  });
-
   return (
     <button id="btn" type="button" className="btn btn-primary">
       Alert
